659226fb793d9e5ca2ec8435378b874d3287d3d6
Aleksandr Osyukov <ocukov@mail.ru>
2023-05-16 22:48:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ug9fx2h6mr9mvon2?svg=true)](https://ci.appveyor.com/project/osyualex/deliverycard)
---
+++ src/test/java/ru/netology/web/DeliverycardTest.java
@@ -0,0 +1,15 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class DeliverycardTest {
+
+    @Test
+    void formAutomationDeliveryCardTest() {
+        open("http://localhost:9999");
+
+    }
+
+}
