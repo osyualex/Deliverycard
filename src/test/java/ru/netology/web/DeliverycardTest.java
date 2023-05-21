09f42a7bb375cd62132f52b4324b9d467305173c
Aleksandr Osyukov <ocukov@mail.ru>
2023-05-21 13:46:31 +0300

    add code commit

--- src/test/java/ru/netology/web/DeliverycardTest.java
+++ src/test/java/ru/netology/web/DeliverycardTest.java
@@ -1,14 +1,47 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.ClickOptions;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.WithText;
 import org.junit.jupiter.api.Test;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.SetValueOptions.withText;
 
 public class DeliverycardTest {
 
+    private String generateDate(int addDays, String pattern){
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
     @Test
     void formAutomationDeliveryCardTest() {
+
+
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        String currentDate = generateDate(4, "dd-MM-yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys((currentDate));
+        $("[data-test-id='name'] input").setValue("Александр Соколов");
+        $("[data-test-id='phone'] input").setValue("+79589999999");
+        $("[data-test-id='agreement'] ").click();
+        $("button.button").click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(new WithText("Встреча ")).should(Condition.appear);
 
     }
 
