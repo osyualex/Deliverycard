403445b4f139141bcf76772e3f4ad4ee1e77f5fa
Aleksandr Osyukov <ocukov@mail.ru>
2023-05-22 22:43:25 +0300

    change code commit

--- src/test/java/ru/netology/web/DeliverycardTest.java
+++ src/test/java/ru/netology/web/DeliverycardTest.java
@@ -21,7 +21,7 @@ import static com.codeborne.selenide.SetValueOptions.withText;
 
 public class DeliverycardTest {
 
-    private String generateDate(int addDays, String pattern){
+    private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
@@ -33,15 +33,16 @@ public class DeliverycardTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
-        String currentDate = generateDate(4, "dd-MM-yyyy");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys((currentDate));
+        $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Александр Соколов");
         $("[data-test-id='phone'] input").setValue("+79589999999");
         $("[data-test-id='agreement'] ").click();
         $("button.button").click();
-        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(new WithText("Встреча ")).should(Condition.appear);
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
 
     }
 
